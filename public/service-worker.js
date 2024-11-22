4511e51d4dafc1ad6076d11ad952206d07124ea2
mohammed daaouan <mohammeddaaouan2018@gmail.com>
2024-11-22 08:44:39 +0100

    fixing notification errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,27 @@
-importScripts("https://js.pusher.com/beams/service-worker.js");
+//importScripts("https://js.pusher.com/beams/service-worker.js");
+// proper initialization
+if( 'function' === typeof importScripts) {
+    /* eslint-disable no-undef */
+    importScripts("https://js.pusher.com/beams/service-worker.js");
+  }
+  console.log('Service worker.js');
+  
+  PusherPushNotifications.onNotificationReceived = ({
+    pushEvent,
+    payload,
+    handleNotification,
+  }) => {
+  
+    console.log("worker got : " + JSON.stringify(payload));
+  
+    // Get the client.
+    self.clients.matchAll().then((matchedClient) => matchedClient.forEach(client => {
+      client.postMessage(payload.data);
+    }));
+  
+    // Your custom notification handling logic here 🛠️
+    // This method triggers the default notification handling logic offered by
+    // the Beams SDK. This gives you an opportunity to modify the payload.
+    // E.g. payload.notification.title = "A client-determined title!"
+    pushEvent.waitUntil(handleNotification(payload));
+  };
